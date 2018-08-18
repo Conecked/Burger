ab6dd28c5c7316ccd68cb33a2eda92564dd37d7f
Armando <xxnowonxx@gmail.com>
2018-08-17 18:31:34 -0700

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Western BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Ahi Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Chicken Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
